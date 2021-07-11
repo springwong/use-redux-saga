8d3cc8ecb75e5669a9efe44c0c45de734461c6cb
Spring <development.spring@gmail.com>
2021-07-12 00:50:50 +0800

    remove unuse method

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
 import {reducerManager, createReducerManager} from "./reducerManager";
-import {sagaManager, createSagaManager, runSaga, setRunSaga} from "./sagaManager";
+import {runSaga, setRunSaga} from "./sagaManager";
 import {useReduxReducer, useReduxReducerLocal, ReduxReducerProvider} from "./useReduxReducer";
 import {useSagaEffect, useSaga, useSagaSimple} from "./useSaga";
 
 export {
     reducerManager,
-    sagaManager,
     useReduxReducer,
     useReduxReducerLocal,
     useSaga,
     useSagaEffect,
     useSagaSimple,
     createReducerManager,
-    createSagaManager,
     ReduxReducerProvider,
     runSaga,
     setRunSaga,
--- src/sagaManager.ts
+++ src/sagaManager.ts
@@ -1,56 +1,56 @@
 
-type SagaManager = {
-    logSaga: () => void;
-    addSaga: (key: string, saga: (params: any) => Generator, params: any) => void,
-    removeSaga: (key: string) => void;
-}
+// type SagaManager = {
+//     logSaga: () => void;
+//     addSaga: (key: string, saga: (params: any) => Generator, params: any) => void,
+//     removeSaga: (key: string) => void;
+// }
 
 export let runSaga: any = undefined;
 export function setRunSaga(middlewareRun: any) {
     runSaga = middlewareRun;
 }
-export let sagaManager: SagaManager | undefined = undefined;
+// export let sagaManager: SagaManager | undefined = undefined;
 
-export function createSagaManager(runSaga: any, rootSaga: () => Generator, runSagaParams: any = {}): SagaManager { // Create a dictionary to keep track of injected sagas
-    const sagas: any = {};
+// export function createSagaManager(runSaga: any, rootSaga: () => Generator, runSagaParams: any = {}): SagaManager { // Create a dictionary to keep track of injected sagas
+//     const sagas: any = {};
 
-    const isInjected = (key: string) => key in sagas;
+//     const isInjected = (key: string) => key in sagas;
 
-    const addSaga = (key: string, saga: (params: any) => Generator, params: any) => { // We won't run saga if it is already injected
-        if (isInjected(key))
-            return;
+//     const addSaga = (key: string, saga: (params: any) => Generator, params: any) => { // We won't run saga if it is already injected
+//         if (isInjected(key))
+//             return;
 
-        // Sagas return task when they executed, which can be used
-        // to cancel them
-        const task = runSaga(saga, params);
+//         // Sagas return task when they executed, which can be used
+//         // to cancel them
+//         const task = runSaga(saga, params);
 
-        // Save the task if we want to cancel it in the future
-        sagas[key] = task;
-    };
+//         // Save the task if we want to cancel it in the future
+//         sagas[key] = task;
+//     };
 
-    const removeSaga = (key: string) => { // We won't run saga if it is already injected
-        if (!isInjected(key))
-            return;
+//     const removeSaga = (key: string) => { // We won't run saga if it is already injected
+//         if (!isInjected(key))
+//             return;
 
-        const task = sagas[key];
-        task.cancel();
+//         const task = sagas[key];
+//         task.cancel();
 
-        delete sagas[key];
-    };
+//         delete sagas[key];
+//     };
 
-    const logSaga = () => {
-        Object.keys(sagas).forEach(key => {
-            console.log(key);
-        });
-    }
+//     const logSaga = () => {
+//         Object.keys(sagas).forEach(key => {
+//             console.log(key);
+//         });
+//     }
 
-    // Inject the root saga as it a staticlly loaded file,
-    addSaga('root', rootSaga, runSagaParams);
+//     // Inject the root saga as it a staticlly loaded file,
+//     addSaga('root', rootSaga, runSagaParams);
 
-    sagaManager = {
-        addSaga,
-        removeSaga,
-        logSaga
-    };
-    return sagaManager;
-}
+//     sagaManager = {
+//         addSaga,
+//         removeSaga,
+//         logSaga
+//     };
+//     return sagaManager;
+// }
